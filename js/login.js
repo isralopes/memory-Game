90202cfa165bc050966b088ca5495f0dac1cb1c8
isralopes <50051343+isralopes@users.noreply.github.com>
2023-05-17 22:13:28 -0300

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('.login__input');
+const button = document.querySelector('.login__button');
+const form = document.querySelector('.login-form');
+
+const validateInput = ({ target }) => {
+  if (target.value.length > 3) {
+    button.removeAttribute('disabled');
+    return;
+  }
+
+  console.log(target)
+
+  button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  localStorage.setItem('player', input.value);
+  window.location = 'pages/game.html';
+  
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
